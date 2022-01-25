b894787f270b586ca05d19dce098ffdfb63c684d
HummusMan <66321101+sohrabhamza@users.noreply.github.com>
2022-01-25 11:10:41 +0530

    Aircontrol and floating improvements
    Organized variables
    Added some user control when player is floating
    Added option for player to have partial control when in the air

--- Global GJ 2022/Assets/Scripts/Devil/DevilController.cs
+++ Global GJ 2022/Assets/Scripts/Devil/DevilController.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class DevilController : MonoBehaviour
 {
-    [Header("Variables")]
+    [Header("Movement")]
     [SerializeField] float speed;
     [SerializeField] float jumpHeight;
-    [SerializeField] Vector2 ZBounds = new Vector2(-5, 5);
+    [SerializeField] bool airControl;
+    [SerializeField] float airSpeed = 3;
+
+    [Header("Floating")]
+    [SerializeField] bool useFloating;
+    // [SerializeField] Vector2 ZBounds = new Vector2(-5, 5);
+    [SerializeField] float normalGravity = 10;
+    [SerializeField] float floatGravity = 2;
+
     //Serializer Refs
     [Header("References")]
     [SerializeField] SpriteRenderer devilSprite;
@@ -21,6 +29,7 @@ public class DevilController : MonoBehaviour
     Vector3 moveDirection;
     bool jumping;
     bool readyToJump = true;
+    bool floating;
 
     private void Start()
     {
@@ -41,7 +50,7 @@ public class DevilController : MonoBehaviour
 
         jumping = Input.GetKey(KeyCode.Space);
 
-        if (grounded)
+        if (grounded || floating || airControl)
         {
             if (x > 0)
             {
@@ -68,6 +77,8 @@ public class DevilController : MonoBehaviour
 
     void Movement()
     {
+        floating = moveDirection.y < 0 && jumping ? true & useFloating : false;
+
         if (grounded)   //If player is grounded
         {
             moveDirection = new Vector3(x * speed, -.75f, /*z * speed*/ 0);   //Set movement vector based on input
@@ -82,12 +93,17 @@ public class DevilController : MonoBehaviour
 
             StartCoroutine(ResetJump());
         }
+        else if (!grounded && (floating || airControl))
+        {
+            moveDirection.x = x * airSpeed;
+            moveDirection = transform.TransformDirection(moveDirection);    //convert to world space
+        }
 
         devilAnimator.SetBool("isGrounded", grounded);
         devilAnimator.SetFloat("YVelocity", moveDirection.y);
 
         //If falling and space button held, make character float
-        moveDirection.y -= (!grounded && moveDirection.y < 0 && jumping ? 2 : 10) * Time.deltaTime;     //Gravity
+        moveDirection.y -= (!grounded && floating ? floatGravity : normalGravity) * Time.deltaTime;     //Gravity
 
         grounded = (controller.Move(moveDirection * Time.deltaTime) & CollisionFlags.Below) != 0;   //Check if grounded
 
