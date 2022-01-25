526c8457e78edf46e64e3d88273748f96e08520a
WillbieChoo <imwilliam99@gmail.com>
2022-01-25 23:46:34 +0000

    Generate Soundbank

--- Global GJ 2022/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Global GJ 2022/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -11,6 +11,17 @@
 
 namespace AK
 {
+    namespace EVENTS
+    {
+        static const AkUniqueID AMBIANCE = 2981377429U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
     namespace BUSSES
     {
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
