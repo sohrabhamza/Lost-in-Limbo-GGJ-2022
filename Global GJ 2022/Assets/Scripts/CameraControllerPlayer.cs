9ce7e3a41cbb68215a98827ef493edccee14e124
surec <sureshchandru2003@gmail.com>
2022-01-26 13:12:31 +0000

    Added smooth camera follow with switching.

--- Global GJ 2022/Assets/Scripts/CameraControllerPlayer.cs
+++ Global GJ 2022/Assets/Scripts/CameraControllerPlayer.cs
@@ -5,13 +5,36 @@ using UnityEngine;
 public class CameraControllerPlayer : MonoBehaviour
 {
     public bool isEnabled;
-    [SerializeField] Transform target;
-    [SerializeField] float distance, height;
-    // Update is called once per frame
-    void Update()
-    {
-        if (!isEnabled) return;
+    [SerializeField] Transform target1;
+    [SerializeField] Transform target2;
 
-        transform.position = target.TransformPoint(0, height, -distance);
+    [SerializeField] Vector3 offset;
+    [SerializeField] float lerpRate;
+
+    private Transform currentTarget;
+
+    // Start is called once
+    private void Start() 
+    {
+        currentTarget = target1;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (isEnabled)
+        {
+            if (Input.GetKeyDown(KeyCode.X))
+            {
+                SwitchTarget();
+            }
+
+            transform.position = Vector3.Lerp(transform.position, currentTarget.position + offset, Time.deltaTime * lerpRate);
+        }
+    }
+
+    private void SwitchTarget() 
+    {
+        currentTarget = currentTarget == target1 ? target2 : target1; 
     }
 }
